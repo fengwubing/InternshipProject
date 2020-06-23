a892c61551f1209ab69c5911a5e655b5412a890e
unknown <fengwubing@gmail.com>
2020-06-23 14:16:35 +0200

    https://github.com/fengwubing/InternshipProject.git

---
+++ Assets/Scripts/GuideRoute.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideRoute : MonoBehaviour
+{
+    public Camera myCamera;
+    private float interactionDistance = 100f;
+    
+    //refer to the living objects
+    public GameObject apple;
+    public GameObject cat;
+    public GameObject carrot;
+    public GameObject chicken;
+    
+    //refer to the non-living objects
+    public GameObject hammer;
+    public GameObject trumpet;
+    public GameObject chair;
+    public GameObject bicycle;
+    
+    //the center points of objects
+    private Vector3 centerApple;
+    private Vector3 centerCat;
+    private Vector3 centerCarrot;
+    private Vector3 centerChicken;
+    
+    private Vector3 centerHammer;
+    private Vector3 centerTrumpet;
+    private Vector3 centerChair;
+    private Vector3 centerBicycle;
+
+    
+    private GameObject lineApple;
+    private GameObject lineCat;
+    private GameObject lineCarrot;
+    private GameObject lineChicken;
+    
+    private GameObject lineHammer;
+    private GameObject lineTrumpet;
+    private GameObject lineChair;
+    private GameObject lineBicycle;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float y = 0.1f;
+        // Vector3 centerApple = apple.GetComponent<MeshRenderer>().bounds.center;
+        // float x = centerApple.x;
+        // float z = centerApple.z;
+        // centerApple=new Vector3(x,y,z);
+        
+        centerApple = ObjectPoint(apple, y);
+        centerCat = ObjectPoint(cat, y);
+        centerCarrot = ObjectPoint(carrot, y);
+        centerChicken = ObjectPoint(chicken, y);
+
+        centerHammer = ObjectPoint(hammer, y);
+        centerTrumpet = ObjectPoint(trumpet, y);
+        centerChair = ObjectPoint(chair, y);
+        centerBicycle = ObjectPoint(bicycle, y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Camera myCamera = GetComponent<Camera>();
+            Vector3 rayOrigin = myCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+
+            RaycastHit hitInfo = new RaycastHit();
+
+            bool hit = Physics.Raycast(rayOrigin, myCamera.transform.forward, out hitInfo, interactionDistance);
+
+            if (hit)
+            {
+                if (hitInfo.transform.CompareTag("APPLE"))
+                {
+                    Debug.Log("I SEE AN APPLE");
+                    Destroy(apple);
+                    Destroy(lineChair);
+                    lineApple=DrawLine(centerApple, centerTrumpet, Color.blue, 1f);
+
+                }
+                else if (hitInfo.transform.CompareTag("CAT"))
+                {
+                    Debug.Log("I SEE A CAT");
+                    Destroy(cat);
+                    Destroy(lineHammer);
+                    lineCat=DrawLine(centerCat, centerBicycle, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("CARROT"))
+                {
+                    Debug.Log("I SEE A CARROT");
+                    Destroy(carrot);
+                    Destroy(lineBicycle);
+                    lineCarrot=DrawLine(centerCarrot, centerChair, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("CHICKEN"))
+                {
+                    Debug.Log("I SEE A CHICKEN");
+                    Destroy(chicken);
+                    Destroy(lineTrumpet);
+                    lineChicken=DrawLine(centerChicken, centerHammer, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("HAMMER"))
+                {
+                    Debug.Log("I SEE A HAMMER");
+                    Destroy(hammer);
+                    Destroy(lineChicken);
+                    lineHammer=DrawLine(centerHammer, centerCat, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("TRUMPET"))
+                {
+                    Debug.Log("I SEE A TRUMPET");
+                    Destroy(trumpet);
+                    Destroy(lineApple);
+                    lineTrumpet=DrawLine(centerTrumpet, centerChicken, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("CHAIR"))
+                {
+                    Debug.Log("I SEE A CHAIR");
+                    Destroy(chair);
+                    Destroy(lineCarrot);
+                    //lineChair=DrawLine(centerChair, centerApple, Color.blue, 1f);
+                }
+                else if (hitInfo.transform.CompareTag("BICYCLE"))
+                {
+                    Debug.Log("I SEE A BICYCLE");
+                    Destroy(bicycle);
+                    Destroy(lineCat);
+                    lineBicycle=DrawLine(centerBicycle, centerCarrot, Color.blue, 1f);
+                }
+            }
+        }
+    }
+
+    GameObject DrawLine(Vector3 startPoint, Vector3 endPoint,Color c,float weight)                              
+    {
+        
+        //define the positions of lines
+        GameObject myLine = new GameObject();
+        myLine.transform.position = startPoint;
+        
+        //add the line renderer component
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        
+        //apply the material in order to show the lines
+        lr.material =new Material(Shader.Find("Mobile/Particles/Additive"));
+        
+        //define the colors of lines
+        lr.startColor = c;
+        lr.endColor = c;
+        
+        //define the weight of lines
+        lr.startWidth = weight;
+        lr.endWidth = weight*2f;
+        
+        //define the point orders of lines
+        lr.SetPosition(0, startPoint);
+        lr.SetPosition(1, endPoint);
+
+        return myLine;
+    }
+    
+    Vector3 ObjectPoint(GameObject gameObject,float yValue) 
+    {
+        //Vector3 gameObjectPoint= gameObject.GetComponent<MeshRenderer>().bounds.center;
+        Vector3 gameObjectPoint = gameObject.transform.position;
+        gameObjectPoint.y = yValue;
+        gameObject.transform.position = gameObjectPoint;
+        
+        return gameObjectPoint;
+    }
+
+    //define the map function
+    float Map(float t, float a1, float b1, float a2, float b2)
+    {
+        if (t >= b1) {
+            return b1;
+        }else if(t<=a1) {
+            return a1;
+        }else {
+            return ((t - a1) * (b2 - a2)) / (b1 - a1) + a2;
+        }
+    }
+}
---
+++ Assets/Scripts/GuideRoute1.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GuideRoute1 : MonoBehaviour
+{
+    public Camera myCamera;
+    private float interactionDistance = 5f;
+    
+    //refer to the living objects
+    public GameObject apple;
+    public GameObject cat;
+    public GameObject carrot;
+    public GameObject chicken;
+    
+    //refer to the non-living objects
+    public GameObject hammer;
+    public GameObject trumpet;
+    public GameObject chair;
+    public GameObject bicycle;
+    
+    //the arrows of living and non-living objects
+    public GameObject arrowAppleTrumpt;
+    public GameObject arrowTrumpetChicken;
+    public GameObject arrowChickenHammer;
+    public GameObject arrowHammerCat;
+    public GameObject arrowCatBicyle;
+    public GameObject arrowBicyleCarrot;
+    public GameObject arrowCarrotChair;
+    public GameObject arrowChairApple;
+
+
+    public Transform player;
+    private float distanceToView = 8f;
+    
+    
+    bool appleVisible = true;
+    bool trumpetVisible = true;
+    bool chickenVisible = true;
+    bool hammerVisible = true;
+    bool catVisible = true;
+    bool bicycleVisible = true;
+    bool carrotVisible = true;
+    bool chairVisible = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make all the arrow invisible
+        arrowAppleTrumpt.GetComponent<Renderer>().enabled = false;
+        arrowTrumpetChicken.GetComponent<Renderer>().enabled = false;
+        arrowChickenHammer.GetComponent<Renderer>().enabled = false;
+        arrowHammerCat.GetComponent<Renderer>().enabled = false;
+        arrowCatBicyle.GetComponent<Renderer>().enabled = false;
+        arrowBicyleCarrot.GetComponent<Renderer>().enabled = false;
+        arrowCarrotChair.GetComponent<Renderer>().enabled = false;
+        arrowChairApple.GetComponent<Renderer>().enabled = false;
+
+        
+        //set the text to default false
+        apple.GetComponentInChildren<TextMeshPro>().enabled=false;
+        cat.GetComponentInChildren<TextMeshPro>().enabled=false;
+        carrot.GetComponentInChildren<TextMeshPro>().enabled=false;
+        chicken.GetComponentInChildren<TextMeshPro>().enabled=false;
+        trumpet.GetComponentInChildren<TextMeshPro>().enabled=false;
+        hammer.GetComponentInChildren<TextMeshPro>().enabled=false;
+        chair.GetComponentInChildren<TextMeshPro>().enabled=false;
+        bicycle.GetComponentInChildren<TextMeshPro>().enabled=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 rayOrigin = myCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(rayOrigin, myCamera.transform.forward, out hitInfo, interactionDistance);
+        
+        
+        //apple
+        if (Vector3.Distance(player.transform.position, apple.transform.position) <= distanceToView)
+        {
+            apple.GetComponentInChildren<TextMeshPro>().enabled = appleVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("APPLE"))
+                    {
+                        Debug.Log("I SEE AN APPLE");
+                        apple.GetComponent<MeshRenderer>().enabled = false;
+                        apple.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowAppleTrumpt.GetComponent<Renderer>().enabled = true;
+                        appleVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            apple.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //trumpet
+        if (Vector3.Distance(player.transform.position, trumpet.transform.position) <= distanceToView)
+        {
+            trumpet.GetComponentInChildren<TextMeshPro>().enabled = trumpetVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("TRUMPET"))
+                    {
+                        Debug.Log("I SEE AN TRUMPET");
+                        trumpet.GetComponent<MeshRenderer>().enabled = false;
+                        trumpet.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowAppleTrumpt.GetComponent<Renderer>().enabled = false;
+                        arrowTrumpetChicken.GetComponent<Renderer>().enabled = true;
+                        trumpetVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            trumpet.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //chicken
+        if (Vector3.Distance(player.transform.position, chicken.transform.position) <= distanceToView)
+        {
+            chicken.GetComponentInChildren<TextMeshPro>().enabled = chickenVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("CHICKEN"))
+                    {
+                        Debug.Log("I SEE AN CHICKEN");
+                        chicken.GetComponent<MeshRenderer>().enabled = false;
+                        chicken.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowTrumpetChicken.GetComponent<Renderer>().enabled = false;
+                        arrowChickenHammer.GetComponent<Renderer>().enabled = true;
+                        chickenVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            chicken.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //hammer
+        if (Vector3.Distance(player.transform.position, hammer.transform.position) <= distanceToView)
+        {
+            hammer.GetComponentInChildren<TextMeshPro>().enabled = hammerVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("HAMMER"))
+                    {
+                        Debug.Log("I SEE AN HAMMER");
+                        hammer.GetComponent<MeshRenderer>().enabled = false;
+                        hammer.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowChickenHammer.GetComponent<Renderer>().enabled = false;
+                        arrowHammerCat.GetComponent<Renderer>().enabled = true;
+                        hammerVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            hammer.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //cat
+        if (Vector3.Distance(player.transform.position, cat.transform.position) <= distanceToView)
+        {
+            cat.GetComponentInChildren<TextMeshPro>().enabled = catVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("CAT"))
+                    {
+                        Debug.Log("I SEE AN CAT");
+                        cat.GetComponent<MeshRenderer>().enabled = false;
+                        cat.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowHammerCat.GetComponent<Renderer>().enabled = false;
+                        arrowCatBicyle.GetComponent<Renderer>().enabled = true;
+                        catVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            cat.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //bicycle
+        if (Vector3.Distance(player.transform.position, bicycle.transform.position) <= distanceToView)
+        {
+            bicycle.GetComponentInChildren<TextMeshPro>().enabled = bicycleVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("BICYCLE"))
+                    {
+                        Debug.Log("I SEE AN BYCYCLE");
+                        bicycle.SetActive(false);
+                        //bicycle.GetComponent<MeshRenderer>().enabled = false;
+                        bicycle.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowCatBicyle.GetComponent<Renderer>().enabled = false;
+                        arrowBicyleCarrot.GetComponent<Renderer>().enabled = true;
+                        bicycleVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            bicycle.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //carrot
+        if (Vector3.Distance(player.transform.position, carrot.transform.position) <= distanceToView)
+        {
+            carrot.GetComponentInChildren<TextMeshPro>().enabled = carrotVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("CARROT"))
+                    {
+                        Debug.Log("I SEE AN CARROT");
+                        carrot.GetComponent<MeshRenderer>().enabled = false;
+                        carrot.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowBicyleCarrot.GetComponent<Renderer>().enabled = false;
+                        arrowCarrotChair.GetComponent<Renderer>().enabled = true;
+                        carrotVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            carrot.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+        
+        
+        //chair
+        if (Vector3.Distance(player.transform.position, chair.transform.position) <= distanceToView)
+        {
+            chair.GetComponentInChildren<TextMeshPro>().enabled = chairVisible;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit)
+                {
+                    if (hitInfo.transform.CompareTag("CHAIR"))
+                    {
+                        Debug.Log("I SEE AN CHAIR");
+                        chair.GetComponent<MeshRenderer>().enabled = false;
+                        chair.GetComponentInChildren<TextMeshPro>().enabled = false;
+                        arrowCarrotChair.GetComponent<Renderer>().enabled = false;
+                        arrowChairApple.GetComponent<Renderer>().enabled = false;
+                        chairVisible = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            chair.GetComponentInChildren<TextMeshPro>().enabled = false;
+        }
+
+
+        
+    }
+}
